55c5bea53c21b7ef40df297198a845012db4f25b
FelipeIzq28 <felipeizquierdo47@gmail.com>
2026-01-18 17:26:20 -0500

    9. Debug Helper

--- Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -3,3 +3,12 @@
 
 #include "Characters/WarriorHeroCharacter.h"
 
+#include "WarriorDebugHelper.h"
+
+
+void AWarriorHeroCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Debug::Print(TEXT("Working"));
+}
